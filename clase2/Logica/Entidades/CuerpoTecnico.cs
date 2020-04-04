e6c1d20c4593b70435dddf1321b41830faf1586a
agustinjacob49 <jacobsen816@gmail.com>
2020-04-03 22:11:25 -0300

    added new entities

---
+++ clase2/Logica/Entidades/CuerpoTecnico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logica
+{
+    class CuerpoTecnico : Persona
+    {
+        private bool esTecnicoPrincipal;
+        private int numeroAyudanteCampo;
+
+        public CuerpoTecnico(int id, int edad, string nombre, string nacionalidad) : base(id, edad, nombre, nacionalidad)
+        {
+        }
+
+        public bool EsTecnicoPrincipal { get => esTecnicoPrincipal; set => esTecnicoPrincipal = value; }
+        public int NumeroAyudanteCampo { get => numeroAyudanteCampo; set => numeroAyudanteCampo = value; }
+    }
+}
